2fc058c05b24691cc4cef2948f715c351b313f2a
PAVI119 <123946240+PAVI119@users.noreply.github.com>
2023-05-12 10:34:31 +0200

    Add files via upload

---
+++ Suma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examenm5pauvizcaino;
+
+/**
+ *
+ * @author PauVizcaino
+ */
+public class Suma {
+
+    int a;
+    int b;
+
+    public int suma(int a, int b) {
+        int suma;
+        suma = a + b;
+        return suma;
+    }
+
+}
